04fb3943a69ea50a28ebd95b0d825b3e2494000c
Michal Plebanski <michal.plebanski@golem.network>
2018-11-24 21:08:58 +0100

    Add CORS

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 from krs import get_krs_obj
@@ -15,10 +17,6 @@ class Company(Resource):
             if int(obj['id']) == company_id:
                 return obj
         # TODO throw error
-
-    def put(self, company_id):
-        companies[company_id] = request.form['data']
-        return {company_id: companies[company_id]}
 
 class Companies(Resource):
     def get(self):
