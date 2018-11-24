45608cda99c67f7e11314c0b0b5d7af2ac891ff5
Michal Plebanski <michal.plebanski@golem.network>
2018-11-24 17:03:42 +0100

    Add flask project

---
+++ flask/openapi_server/controllers/default_controller.py
@@ -0,0 +1,46 @@
+import connexion
+import six
+
+from openapi_server.models.companies import Companies  # noqa: E501
+from openapi_server.models.company import Company  # noqa: E501
+from openapi_server.models.error import Error  # noqa: E501
+from openapi_server import util
+
+
+def companies_get():  # noqa: E501
+    """Gets some companies
+
+    Returns a list containing all companies. The list supports paging. # noqa: E501
+
+
+    :rtype: Companies
+    """
+    return 'do some magic!'
+
+
+def companies_id_get(id):  # noqa: E501
+    """Gets a company
+
+    Returns a single company for its id. # noqa: E501
+
+    :param id: The company&#39;s id
+    :type id: 
+
+    :rtype: Company
+    """
+    return 'do some magic!'
+
+
+def companies_post(company=None):  # noqa: E501
+    """Creates a company
+
+    Adds a new person to the persons list. # noqa: E501
+
+    :param company: The company to create.
+    :type company: dict | bytes
+
+    :rtype: None
+    """
+    if connexion.request.is_json:
+        company = Company.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
---
+++ flask/openapi_server/models/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from openapi_server.models.companies import Companies
+from openapi_server.models.company import Company
+from openapi_server.models.error import Error
