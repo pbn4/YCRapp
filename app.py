00bf1c2af74485452b30b81a090d195f4e0acb79
Marcin Kraszewski <mrkraszewski@gmail.com>
2018-11-24 21:09:51 +0100

    updated freeze file

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from similarity.normalized_levenshtein import NormalizedLevenshtein
 
 app = Flask(__name__)
 api = Api(app)
@@ -18,15 +19,28 @@ class Company(Resource):
         # TODO throw error
 
     def put(self, company_id):
-        companies[company_id] = request.form['data']
+        companies[company_idcompany_id] = request.form['data']
         return {company_id: companies[company_id]}
 
 class Companies(Resource):
     def get(self):
         return companies_arr
 
+class Search(Resource):
+
+    def get(self, company_name):
+        nl = NormalizedLevenshtein()
+        allNames = [(nl.distance(company_name.upper(), a['nazwa']), a['id'], a['nazwa']) for a in companies_arr]
+        best_result = sorted(allNames,reverse=True)[-1]
+        if best_result[0] > 0.1: #result is bad
+            return {'Error':'Company not found!'}
+        else:
+            return {'Closest Match':best_result}
+            
+
 api.add_resource(Company, '/companies/<int:company_id>')
 api.add_resource(Companies, '/companies')
+api.add_resource(Search, '/search/<string:company_name>')
 
 if __name__ == '__main__':
     app.run(debug=True)
