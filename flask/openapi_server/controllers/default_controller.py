ecebb60a4ae553816c77b1d679253ea875345c9c
Michal Plebanski <michal.plebanski@golem.network>
2018-11-24 18:58:36 +0100

    Add companies data filtered

--- flask/openapi_server/controllers/default_controller.py
+++ flask/openapi_server/controllers/default_controller.py
@@ -11,7 +11,21 @@ import json
 with open('krs.json', 'r') as f:
     krs_json = json.loads(f.read())
 
-krs = krs_json['Dataobject']
+def filter_krs_obj(krs):
+    out_objs = []
+
+    krs = krs_json['Dataobject']
+    for obj in krs:
+        out = {}
+        out['id'] = obj['id']
+        out['url'] = obj['url']
+        out['nazwa'] = obj['data']['krs_podmioty.nazwa']
+        out['nip'] = obj['data']['krs_podmioty.nip']
+        out['regon'] = obj['data']['krs_podmioty.regon']
+        out['date_created'] = obj['data']['krs_podmioty.data_dokonania_wpisu']
+        out_objs.append(out)
+
+    return out_objs
 
 def companies_get():  # noqa: E501
     """Gets some companies
@@ -21,7 +35,7 @@ def companies_get():  # noqa: E501
 
     :rtype: Companies
     """
-    return krs
+    return filter_krs_obj(krs_json)
 
 
 def companies_id_get(id):  # noqa: E501
